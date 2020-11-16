77c456d0087299874d084af8470e791524eaae27
williams2920 <56734372+williams2920@users.noreply.github.com>
2020-11-16 13:44:09 -0700

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,25 @@
+
+function ver(n) {
+         document.getElementById("subseccion"+n).style.display="block"
+         
+         }
+function ocultar(n) {
+         document.getElementById("subseccion"+n).style.display="none"
+         }
+//<script src="intro.js"></script>
+//alert("quiubo prrrrro");
+//variable
+//var nombre=will;
+//leer
+//nombre=prompt("ingresa tu nombre", "valor");
+//edad=prompt("ingresar edad")
+//mostrar
+//alert(nombre);
+//document.write(nombre+"  "+edad);
+//se utiliza el inpector para ver el console
+//console.log(nombre);
+//scrol 
+//width: 200px;
+//height: 200px;
+// background-color: #eee;
+// overflow: scroll;
